799d5f1f8327c364ad0b1e1555246b31e45c2ca7
Sayak123456 <nandysayak552@gmail.com>
2024-06-09 17:41:55 +0530

    Update Login Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -17,7 +17,7 @@ const LoginLayout = styled.main`
 function Login() {
   return <LoginLayout>
     <Logo />
-    <Heading as="h4">Log in to account</Heading>
+    <Heading as="h4">Log in to your account</Heading>
     <LoginForm />
   </LoginLayout>;
 }
